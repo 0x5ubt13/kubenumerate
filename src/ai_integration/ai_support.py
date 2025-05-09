c6bb9e7b1faa5ad05e388352f5bb2bff8bc6564c
0x5ubt13 <5ubt13@protonmail.com>
2025-05-09 15:19:24 +0100

    Refactor test Chatbot system message for improved clarity and context; update test file path for user input.

--- src/ai_integration/ai_support.py
+++ src/ai_integration/ai_support.py
@@ -20,7 +20,9 @@ class Chatbot:
         requests.packages.urllib3.disable_warnings(InsecureRequestWarning)
 
         messages = [
-            {"role": "system", "content": user_input}
+            # ["role": "system", "content": "You are a pentester doing an assessment. Given the 'true' values, write high level issues explaining "],
+            {"role": "system", "content": "You are a highly technical quality assurance professional working in a UK-based cyber security consultancy. You will be provided individual issue text to QA for penetration testing reports, these will be part of a larger report which contains a number of issues. Respond concisely and clearly and limit the length of the reply. Include ideas on how to improve the text, or fix errors or ambiguity. Do not provide full example text to replace the input, you are to advise and give ideas only. Do not repeat or summarise the text back to the user, it is not required. Your output will be used as a comment in a Word document, and should be appropriate for such usage"},
+            {"role": "user", "content": user_input}
         ]
 
         payload = {"model": "gpt-4o", "messages": messages}
@@ -29,7 +31,7 @@ class Chatbot:
         response = requests.post(self.url, json=payload, headers=headers, verify=False)
         if response.status_code == 200:
             return response.json().get("choices", [{}])[0].get("message", {}).get("content", "No response content found.")
+            # return response.json()
         else:
             print(f"Error: {response.status_code} - {response.text}")
             return "Error: Unable to connect to the chatbot."
-
--- src/ai_integration/ai_test.py
+++ src/ai_integration/ai_test.py
@@ -4,9 +4,9 @@ import os
 def main():
     chatbot = Chatbot()
 
-    with open(os.path.expanduser("~") + "/transcript_test.txt") as f:
+    with open(os.path.expanduser("~") + "/kubenumerate/test_issues_dict.txt") as f:
         user_input = [line for line in f]
-
+    
     print(chatbot.chat(user_input))
 
 if __name__ == "__main__":
